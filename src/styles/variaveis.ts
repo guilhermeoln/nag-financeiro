3fcd024c61152662bced931de74763ba222569f7
jose guilherme <guilhermedev2002@gmail.com>
2023-07-10 17:13:34 -0300

    first commit

---
+++ src/styles/variaveis.ts
@@ -0,0 +1,8 @@
+export const grey = "#252C32";
+export const black900 = "#18181B";
+export const green700 = "#119C2B";
+export const green100 = "#EBFFF1";
+export const orange900 = "#AE4408";
+export const orange100 = "#F5F0E2";
+export const blue900 = "#1971C2";
+export const blue100 = "#D7EDFF";
