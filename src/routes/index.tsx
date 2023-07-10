8fca284a00ab38574e44922a23cd86a7f6ed3956
jose guilherme <guilhermedev2002@gmail.com>
2023-07-10 17:42:35 -0300

    feat(routes): Criação da rota de login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/login";
 
 export default function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>Olá</>} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
