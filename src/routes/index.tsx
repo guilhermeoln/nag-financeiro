f6fc855032c55a5503d0ebeed696e7a6b5fe247c
jose guilherme <guilhermedev2002@gmail.com>
2023-07-10 20:57:39 -0300

    fix(routes): Colocando o componente NavigationBar fixo em todas as rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
+import Home from "../pages/home";
+import { Flex } from "@chakra-ui/react";
+import NavigationBar from "../components/NavigationBar";
 
 export default function RoutesApp() {
   return (
     <BrowserRouter>
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/inicio" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
