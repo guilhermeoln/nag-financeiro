24aad147e904fbd4f5e777d635886a2d207e6b26
jose guilherme <guilhermedev2002@gmail.com>
2023-07-10 21:36:05 -0300

    fix(mock): Adicionando icones ativos e inativos no mock de navegação

--- src/mock/navigationBar.ts
+++ src/mock/navigationBar.ts
@@ -1,30 +1,38 @@
 import INavigationBar from "../types/INavigationBar";
+import iconCashNavbar from "../assets/iconCashNavbar.png";
+import iconHomeNavbarActive from "../assets/iconHomeNavbarActive.png";
+import iconHomeNavbarInactive from "../assets/iconHomeNavbarInactive.png";
+import iconCashNavbarActive from "../assets/iconCashNavbarActive.png";
 
 const navigationBar: INavigationBar[] = [
-  { id: 1, name: "Início", icon: "/inicio", path: "/inicio" },
+  {
+    id: 1,
+    name: "Início",
+    iconActive: `${iconHomeNavbarActive}`,
+    iconInactive: `${iconHomeNavbarInactive}`,
+    path: "/inicio",
+  },
   {
     id: 2,
     name: "Despesas",
-    icon: "/despesas",
+    iconActive: `${iconCashNavbarActive}`,
+    iconInactive: `${iconCashNavbar}`,
     path: "/despesas",
     subItems: [
       {
         id: 1,
         name: "Entradas",
-        icon: "/despesas",
         path: "/despesas/entradas",
       },
-      { id: 2, name: "Saídas", icon: "/despesas", path: "/despesas/saidas" },
+      { id: 2, name: "Saídas", path: "/despesas/saidas" },
       {
         id: 3,
         name: "Criar entrada",
-        icon: "/despesas",
         path: "/despesas/criar-entrada",
       },
       {
         id: 4,
         name: "Criar saída",
-        icon: "/despesas",
         path: "/despesas/criar-saida",
       },
     ],
