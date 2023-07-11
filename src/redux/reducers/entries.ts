f9a077db347eaf71edb65db591de9277c8f80cc2
jose guilherme <guilhermedev2002@gmail.com>
2023-07-11 14:30:17 -0300

    fix(reducers): inserindo propriedade ID nas entradas

--- src/redux/reducers/entries.ts
+++ src/redux/reducers/entries.ts
@@ -3,18 +3,21 @@ import IEntries from "../../types/IEntries";
 
 const initialState: IEntries[] = [
   {
+    id: 1,
     issuer: "José Guilherme",
     status: "Pendente",
     date: "12/06/2023",
     value: 850,
   },
   {
+    id: 2,
     issuer: "José Guilherme",
     status: "Confirmado",
     date: "02/06/2023",
     value: 920,
   },
   {
+    id: 3,
     issuer: "Nalbert Cezar",
     status: "Pendente",
     date: "25/06/2023",
