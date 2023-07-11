e0b778c158df9e8d650c199452d0b78e1158ce0e
jose guilherme <guilhermedev2002@gmail.com>
2023-07-11 14:30:54 -0300

    feat(routes): Inserindo rota de despesas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import Login from "../pages/login";
 import Home from "../pages/home";
 import { Flex } from "@chakra-ui/react";
 import NavigationBar from "../components/NavigationBar";
+import Expenses from "../pages/expenses";
 
 export default function RoutesApp() {
   return (
@@ -13,6 +14,7 @@ export default function RoutesApp() {
       <NavigationBar />
       <Routes>
         <Route path="/inicio" element={<Home />} />
+        <Route path="/despesas" element={<Expenses />} />
       </Routes>
     </BrowserRouter>
   );
