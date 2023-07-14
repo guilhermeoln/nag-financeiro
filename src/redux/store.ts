e647456bdff7227c1307f16af4e831a2ca537e3e
jose guilherme <guilhermedev2002@gmail.com>
2023-07-14 00:55:03 -0300

    feat(store): Inserindo o reducer do usuário

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import entriesSlice from "./reducers/entries";
 import exitsSlice from "./reducers/exits";
+import userSlice from "./reducers/user";
 
 const store = configureStore({
   reducer: {
     entries: entriesSlice,
     exits: exitsSlice,
+    user: userSlice,
   },
 });
 
