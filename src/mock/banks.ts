9a77cf4c1b90803942dd4e5e38763639904a66bb
jose guilherme <guilhermedev2002@gmail.com>
2023-07-14 14:50:56 -0300

    feat(mock): criando lista dos bancos

---
+++ src/mock/banks.ts
@@ -0,0 +1,9 @@
+import IBanks from "../types/IBanks";
+
+const banks: IBanks[] = [
+  { id: 1, name: "Banco do Brasil" },
+  { id: 2, name: "Bradesco" },
+  { id: 3, name: "Santander" },
+];
+
+export default banks;
