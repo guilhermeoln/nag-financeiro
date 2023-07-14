16f465baa8af1de028d165a7866e92844878aa99
jose guilherme <guilhermedev2002@gmail.com>
2023-07-14 12:24:02 -0300

    feat(mock): Criando moc dos tipos de pagamento

---
+++ src/mock/typesPayment.ts
@@ -0,0 +1,9 @@
+import ITypesPayment from "../types/ITypesPayment";
+
+const typesPayment: ITypesPayment[] = [
+  { id: 1, name: "Pix" },
+  { id: 2, name: "Boleto" },
+  { id: 2, name: "Cheque" },
+];
+
+export default typesPayment;
